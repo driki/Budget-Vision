116d2e00d6eedcb1f3ce7513703e3e75651cc429
Matt MacDonald <matt.macdonald@gmail.com>
2012-01-29 08:53:58 -0500

    Formatting clean up and trying to fix the is_expense boolean heroku PG issue.

--- db/migrate/20120125115705_add_is_expense_to_line_item.rb
+++ db/migrate/20120125115705_add_is_expense_to_line_item.rb
@@ -1,5 +1,5 @@
 class AddIsExpenseToLineItem < ActiveRecord::Migration
   def change
-  	add_column :items, :is_expense, :boolean, :default => 1
+  	add_column :items, :is_expense, :boolean, :default => true
   end
 end
