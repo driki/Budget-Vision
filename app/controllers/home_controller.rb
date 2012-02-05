d6f1ac672d53f7a5aa6fc2217b7a0c496e465555
Matt MacDonald <matt.macdonald@gmail.com>
2012-02-05 09:48:56 -0500

    Using slugs for finding examples.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @featured_city_0 = Organization.where(:name => "Hermon", :state => "ME").first
-    @featured_city_1 = Organization.where(:name => "Mount Desert", :state => "ME").first
-    @featured_city_2 = Organization.where(:name => "Watertown Town", :state => "MA").first
+    @featured_city_0 = Organization.find_by_slug("hermon-me")
+    @featured_city_1 = Organization.find_by_slug("mount-desert-me")
+    @featured_city_2 = Organization.find_by_slug("watertown-ma")
 
-    @example_city_0 = Organization.where(:name => "Union", :state => "CT").first
-    @example_city_1 = Organization.where(:name => "Salisbury", :state => "NH").first
-    @example_city_2 = Organization.where(:name => "Hermon", :state => "ME").first
-    @example_city_3 = Organization.where(:name => "Belmont", :state => "MA").first
-    @example_city_4 = Organization.where(:name => "Waltham", :state => "MA").first
-    @example_city_5 = Organization.where(:name => "Boston", :state => "MA").first
+    @example_city_0 = Organization.find_by_slug("union-ct")
+    @example_city_1 = Organization.find_by_slug("salisbury-nh")
+    @example_city_2 = Organization.find_by_slug("hermon-me")
+    @example_city_3 = Organization.find_by_slug("belmont-ma")
+    @example_city_4 = Organization.find_by_slug("waltham-ma")
+    @example_city_5 = Organization.find_by_slug("boston-ma")
   end
 
   def about
