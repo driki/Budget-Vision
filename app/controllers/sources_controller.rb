139979350afab46d3558f1e01f7b5a3a7d858f96
Matt MacDonald <matt.macdonald@gmail.com>
2012-02-05 12:21:49 -0500

    Getting TreeMap working

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -13,7 +13,7 @@ class SourcesController < ApplicationController
   	@source = Source.new(params[:source])
   	@project.sources << @source
     if @project.save
-      redirect_to project_sources_path(@source.project, @source)
+      redirect_to organization_project_sources_path(@organization, @project, @source)
     else
       render :action => 'new'
     end
@@ -28,7 +28,7 @@ class SourcesController < ApplicationController
   def update
     respond_to do |format|
       if @source.update_attributes(params[:source])
-        format.html { redirect_to project_sources_path(@project, :notice => 'source was successfully updated.') }
+        format.html { redirect_to organization_project_sources_path(@organization, @project, :notice => 'source was successfully updated.') }
         format.json { respond_with_bip(@source) }
       else
         format.html { render :action => "edit" }
