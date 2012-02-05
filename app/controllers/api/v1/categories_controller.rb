0884953ac1b83bf5d27c34ff44b06a85287f7740
Matt MacDonald <matt.macdonald@gmail.com>
2012-02-05 14:14:28 -0500

    More tweaking of the UI. Got the treemaps working for Overview. Now onto making categories, items and other pages a bit more compelling.

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -3,8 +3,11 @@ class Api::V1::CategoriesController < ApplicationController
   def show
     respond_to do |format|
       format.json{
-        @expense_categories = Project.find_by_id(params[:id]).categories.where(:is_expense => true).roots
-        @revenue_categories = Project.find_by_id(params[:id]).categories.where(:is_expense => false).roots
+      	is_expense = false
+      	if params[:is_expense]
+      		is_expense = true
+      	end
+        @categories = Project.find_by_id(params[:id]).categories.where(:is_expense => is_expense).roots
         render :partial => "api/v1/category.json"
       }
     end
